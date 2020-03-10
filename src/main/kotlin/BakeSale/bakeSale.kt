0e0871504ee246193f46bad3f2cbbcdab0bf82cb
julie.woodford <julie.woodford@johnlewis.co.uk>
2020-03-10 08:11:01 +0000

    refactor for DRY

--- src/main/kotlin/BakeSale/bakeSale.kt
+++ src/main/kotlin/BakeSale/bakeSale.kt
@@ -1,31 +1,33 @@
 package BakeSale
 
-fun checkout(veganCakes: Int, nonVeganCakes: Int): Double {
+data class Charges(
+    val chargeForGroups: Double = 0.0,
+    var remainderOf3: Int = 0
+)
 
-    val remainderOf15 = nonVeganCakes % 15
-    val groupsOf15 = nonVeganCakes / 15
+fun chargeForCakes(cakes: Int, groupOf15Charge: Double, groupOf5Charge: Double, groupOf3Charge: Double): Charges {
+    val remainderOf15 = cakes % 15
+    val groupsOf15 = cakes / 15
     val remainderOf5 = remainderOf15 % 5
     val groupsOf5 = remainderOf15 / 5
-    var remainderOf3 = remainderOf5 % 3
+    val remainderOf3 = remainderOf5 % 3
     val groupsOf3 = remainderOf5 / 3
+    val chargeForGroups = (groupsOf15 * groupOf15Charge) + (groupsOf5 * groupOf5Charge) + (groupsOf3 * groupOf3Charge)
+    return Charges(chargeForGroups, remainderOf3)
+}
 
-    val remainderOfVegan15 = veganCakes % 15
-    val groupsOfVegan15 = veganCakes / 15
-    val remainderOfVegan5 = remainderOfVegan15 % 5
-    val groupsOfVegan5 = remainderOfVegan15 / 5
-    var remainderOfVegan3 = remainderOfVegan5 % 3
-    val groupsOfVegan3 = remainderOfVegan5 / 3
+fun checkout(veganCakes: Int, nonVeganCakes: Int): Double {
+    val chargesForNonVeganCake = chargeForCakes(nonVeganCakes, 5.0, 3.0, 2.0)
+    val chargesForVeganCakes = chargeForCakes(veganCakes, 6.0, 4.0, 3.0)
 
-    val mixedGroupsOf3 = (remainderOf3 + remainderOfVegan3) / 3
+    val mixedGroupsOf3 = (chargesForNonVeganCake.remainderOf3 + chargesForVeganCakes.remainderOf3) / 3
 
     if (mixedGroupsOf3 > 0) {
-        remainderOfVegan3 = (remainderOf3 + remainderOfVegan3) % 3
-        remainderOf3 = 0
+        chargesForVeganCakes.remainderOf3 =
+            (chargesForNonVeganCake.remainderOf3 + chargesForVeganCakes.remainderOf3) % 3
+        chargesForNonVeganCake.remainderOf3 = 0
     }
 
-    return (groupsOf15 * 5) + (groupsOf5 * 3) + (groupsOf3 * 2) +
-            (groupsOfVegan15 * 6) + (groupsOfVegan5 * 4) + (groupsOfVegan3 * 3) +
-            (mixedGroupsOf3 * 2.4) +
-            remainderOf3 + (remainderOfVegan3 * 1.5)
-
+    return chargesForNonVeganCake.chargeForGroups + chargesForVeganCakes.chargeForGroups + (mixedGroupsOf3 * 2.4) +
+            chargesForNonVeganCake.remainderOf3 + (chargesForVeganCakes.remainderOf3 * 1.5)
 }
